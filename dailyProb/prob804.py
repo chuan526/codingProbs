c3780d5c3f85a96883430b26197da899863ad3e5
cy <cy5262014@gmail.com>
2019-08-14 00:04:04 -0700

    Create prob804.py

---
+++ dailyProb/prob804.py
@@ -0,0 +1,32 @@
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+# Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+# Return the number of different transformations among all words we have.
+
+# Example:
+# Input: words = ["gin", "zen", "gig", "msg"]
+# Output: 2
+# Explanation: 
+# The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+
+# There are 2 different transformations, "--...-." and "--...--.".
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        uniq_concat_codes = set()
+        
+        for word in words: 
+            word_code = ''
+            for c in word.lower():
+                word_code += codes[ord(c)-97]
+            uniq_concat_codes.add(word_code)
+        return len(uniq_concat_codes)
