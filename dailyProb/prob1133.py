660002139efc49c37e1dcf3762eee8efe966230d
cy <8106804+chuan526@users.noreply.github.com>
2019-08-22 22:57:53 -0700

    Create prob1133.py

---
+++ dailyProb/prob1133.py
@@ -0,0 +1,28 @@
+# Given an array of integers A, return the largest integer that only occurs once.
+
+# If no integer occurs once, return -1.
+
+ 
+
+# Example 1:
+
+# Input: [5,7,3,9,4,9,8,3,1]
+# Output: 8
+# Explanation: 
+# The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it's the answer.
+# Example 2:
+
+# Input: [9,9,8,8]
+# Output: -1
+# Explanation: 
+# There is no number that occurs only once.
+#  
+from collections import Counter 
+class Solution:
+    def largestUniqueNumber(self, A: List[int]) -> int:
+        counter = Counter(A)
+        res = -1
+        for k,v in counter.items():
+            if v == 1:
+                res = max(res, k)
+        return res        
