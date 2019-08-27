7d0faf46d709567a7c01d6361e337f7fe76347a3
cy <8106804+chuan526@users.noreply.github.com>
2019-08-26 23:22:06 -0700

    Create prob1078.py

---
+++ dailyProb/prob1078.py
@@ -0,0 +1,23 @@
+# Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+
+# For each such occurrence, add "third" to the answer, and return the answer.
+
+ 
+
+# Example 1:
+
+# Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+# Output: ["girl","student"]
+# Example 2:
+
+# Input: text = "we will we will rock you", first = "we", second = "will"
+# Output: ["we","rock"]
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split(' ')
+        res = [] 
+        for i in range(len(words)-2): 
+            if words[i] == first and words[i+1] == second:
+                res.append(words[i+2])
+        return res
